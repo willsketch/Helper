8d4edcc99ec3f494fa2b29c1b7bd6e80f09becad
william mwine <williammwine219@gmail.com>
2022-11-17 00:03:20 +0100

    modified main.py

--- helper/main.py
+++ helper/main.py
@@ -28,12 +28,3 @@ class Helper():
             stop="\ninput:"
         )
         return response
-
-    # def cli(self):
-    #     openai.api_key = os.getenv("OPENAI_API_KEY")
-    #     parser = argparse.ArgumentParser()
-    #     parser.add_argument('prompt', help='What do you need help with', default= '')
-    #     self.prompt= parser.parse_args().prompt
-
-    #     response = self.run_engine()
-    #     print(response.choices[0].text)
