a2a772e8560e40ee3d9d3b09618209298bd13ebd
william mwine <williammwine219@gmail.com>
2022-11-19 13:38:53 +0100

    having troubles with pypi

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("Description.rst", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read()
 
 setup(
- name= 'helper',
- version= '0.0.3',
+ name= 'cli-assistant',
+ version= '0.0.5',
  author= 'william mwine',
  author_email= 'williammwine219@gmail.com',
  license= 'MIT License',
  description='guide you with terminal and git commands',
  long_description=long_description,
  url='https://github.com/willsketch/Helper',
- py_modules=['helper', 'my_helper'],
+ py_modules=[ 'my_helper'],
  packages= find_packages(),
  install_requires = [requirements],
  classifiers=[
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.8.12',
+    'Programming Language :: Python :: 3.8',
     ],
  include_package_data=True,
  package_data={'helper':['examples.txt']},
